11a9b1ae0f5553025658a89d3c79a66776bb385a
moong <taggingmon@gmail.com>
2025-06-07 22:31:24 +0900

    feat: rust ts-rs 추가 및 Response 적용, package.json script 수정

--- bin/ssh-add-moong.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-eval $(ssh-agent -s); ssh-add ~/.ssh/taggingmon_rsa; ssh -T git@github.com;
--- src-tauri/src/common/response.rs
+++ src-tauri/src/common/response.rs
@@ -1,13 +1,16 @@
 use serde::Serialize;
+use ts_rs::TS;
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, TS)]
+#[ts(export)]
 pub enum ResponseStatus {
   Success,
   Canceled,
   Failed,
 }
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, TS)]
+#[ts(export)]
 pub struct Response<T> {
   pub status: ResponseStatus,
   pub message: Option<String>,
