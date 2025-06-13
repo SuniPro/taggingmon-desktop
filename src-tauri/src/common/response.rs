b435abb15bf8bb338b19c005e21c37719e28e3c4
Ariel-250225 <seobin250225@icloud.com>
2025-06-13 19:55:03 +0700

    feature : 파일 응답 설계
    ok, fail, cancel 총 세가지로 구분함

--- src-tauri/src/common/response.rs
+++ src-tauri/src/common/response.rs
@@ -16,3 +16,29 @@ pub struct Response<T> {
   pub message: Option<String>,
   pub data: Option<T>,
 }
+
+impl<T> Response<T> {
+  pub fn ok(data: T) -> Self {
+    Self {
+      status: ResponseStatus::Success,
+      message: None,
+      data: Some(data),
+    }
+  }
+
+  pub fn fail(message: String) -> Self {
+    Self {
+      status: ResponseStatus::Failed,
+      message: Some(message),
+      data: None,
+    }
+  }
+
+  pub fn cancel(message: String) -> Self {
+    Self {
+      status: ResponseStatus::Canceled,
+      message: Some(message),
+      data: None,
+    }
+  }
+}
