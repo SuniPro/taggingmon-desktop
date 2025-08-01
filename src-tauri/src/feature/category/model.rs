aa175be0e4482f5306a55a473cb9a0530756b841
Ariel-250225 <seobin250225@icloud.com>
2025-08-01 20:04:30 +0700

    feature : category crud 추가

---
+++ src-tauri/src/feature/category/model.rs
@@ -0,0 +1,16 @@
+use serde::{Deserialize, Serialize};
+use ts_rs::TS;
+use crate::feature::tag::Tag;
+
+/** Tag를 그룹화 하는 객체입니다.
+    자녀요소로 Tag를 가지고 있으며.
+    카테고리를 호출 시 모든 태그를 함께 호출합니다.
+*/
+#[derive(Debug, Serialize, Deserialize, TS)]
+#[ts(export)]
+pub struct Category {
+    pub id: i64,
+    pub name: String,
+    pub is_default: String,
+    pub tags: Option<Vec<Tag>>,
+}
