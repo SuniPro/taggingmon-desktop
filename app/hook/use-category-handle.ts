55a0b767b9adadc43bbb07f94afe816e0282d989
Ariel-250225 <seobin250225@icloud.com>
2025-08-12 20:33:01 +0700

    refactor : category 추가를 위해 파라미터 이름 변경

--- app/hook/use-category-handle.ts
+++ app/hook/use-category-handle.ts
@@ -3,18 +3,18 @@ import { invokeData } from '~/response/response';
 import type { Category } from '../../src-tauri/bindings/Category';
 import type { FsoInfo } from '../../src-tauri/bindings/FsoInfo';
 
-export type CategoryInput = { name: string; isDefault: boolean };
+export type CategoryInput = { name: string; is_default: boolean };
 
 export const AutoCategoriesAndInfoGenerator = (
 	fsoList: FsoInfo[],
-): { name: string; isDefault: boolean; fsoInfo: FsoInfo }[] => {
+): { name: string; is_default: boolean; fsoInfo: FsoInfo }[] => {
 	const seen = new Set<string>();
 
 	return fsoList
 		.filter(d => d.created_at)
 		.map(info => {
 			const dt = parseFileSystemTime(info.created_at!);
-			return { name: formatDateToYYYYMM(dt), isDefault: true, fsoInfo: info };
+			return { name: formatDateToYYYYMM(dt), is_default: true, fsoInfo: info };
 		})
 		.filter(c => (seen.has(c.name) ? false : (seen.add(c.name), true)));
 };
